ed191e7c8bacbe4904cba3afa0c609159b536d9e
Koraboevvv <bexruzkarabaev@gmail.com>
2025-05-05 16:47:44 +0500

    m

--- js/app.js
+++ js/app.js
@@ -13,5 +13,39 @@
         return(n.slice(0, 5)) 
     }
     console.log(A2("better lorem home"));
+}
+
+{
+    // M-3
+    function A3(n) {
+        return(n.includes("text", "world"))
+        
+    }
+    console.log(A3("hello y text best in the world"));
     
+}
+
+// {
+//     // M-4
+//     function A4(n) {
+//         return
+//     }
+//     console.log(A3());
+//  
+// }
+
+{
+    // M-5
+    function A5(n) {
+        return(n.replace("Bugun", "Ertaga"))
+    }
+    console.log(A5("Bugun men maktabga boraman"));
+}
+
+{
+    // M-6
+    function A6(n) {
+        return(n.slice(2,5).toUpperCase())
+    }
+    console.log(A6("salom"));
 }
