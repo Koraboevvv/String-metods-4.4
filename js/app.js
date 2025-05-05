18e488349c52f2f6a4f7381f38030ae921c2e473
Koraboevvv <bexruzkarabaev@gmail.com>
2025-05-05 16:32:05 +0500

    hom

---
+++ js/app.js
@@ -0,0 +1,17 @@
+{
+    // M-1
+    function A1(n) {
+        return( n.repeat(3) );
+        
+    }
+    console.log(A1("hello"));    
+}
+
+{
+    // M-2
+    function A2(n) {
+        return(n.slice(0, 5)) 
+    }
+    console.log(A2("better lorem home"));
+    
+}
